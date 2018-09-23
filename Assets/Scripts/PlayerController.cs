44ccf9183091cfa72f610d89723389c19a0a38e0
FranLenny <42728905+FranLenny@users.noreply.github.com>
2018-09-23 15:47:52 -0400

    Challenge

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,20 +3,25 @@ using UnityEngine.UI;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
     public float speed;
     public Text countText;
     public Text winText;
+    public Text scoreText;
 
     private Rigidbody rb;
     private int count;
+    private int score;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         count = 0;
+        score = 0;
         SetCountText ();
+        SetScoreText ();
         winText.text = "";
     }
 
@@ -33,22 +38,43 @@ public class PlayerController : MonoBehaviour {
         rb.AddForce(movement * speed);
     }
 
-    void OnTriggerEnter(Collider other)
+   private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("PickUp"))
         {
             other.gameObject.SetActive(false);
             count = count + 1;
-            SetCountText ();
+            score = score + 1;
+            SetAllText();
+        }
+        else if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.SetActive(false);
+            // count = count + 1;
+            score = score - 1; 
+            SetAllText();
+        }
+        if (count == 12)
+        {
+            transform.position = new Vector3(37.5f, 22f, -8.7f);
         }
     }
 
     void SetCountText ()
     {
+        countText.text = "count: " + count.ToString();   
+    }
+    void SetScoreText ()
+    {
+        scoreText.text = "score: " + count.ToString();
+    }
+    void SetAllText ()
+    {
+        scoreText.text = "score: " + score.ToString();
         countText.text = "count: " + count.ToString();
-        if (count >= 12)
+        if (count >= 24)
         {
-            winText.text = "You Win!";
+            winText.text = "You finished with a score of: " + score.ToString();
         }
     }
 }
